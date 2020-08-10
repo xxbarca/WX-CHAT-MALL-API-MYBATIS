df4e45c004fb6b12d29ddbdb844711c4bd1315fa
liyang <xxbarca@163.com>
2020-08-10 15:29:56 +0800

    修改小bug

--- src/main/java/com/ly/imallbatis/api/v1/TagController.java
+++ src/main/java/com/ly/imallbatis/api/v1/TagController.java
@@ -2,22 +2,22 @@ package com.ly.imallbatis.api.v1;
 
 import com.ly.imallbatis.model.Tag;
 import com.ly.imallbatis.service.TagService;
-import com.ly.imallbatis.service.impl.TagServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@ResponseBody
+@RestController
 @RequestMapping(value = "/tag")
 public class TagController {
 
     @Autowired
     private TagService tagService;
 
-    @RequestMapping("/v1/type/{type}")
+    @RequestMapping("/type/{type}")
     public List<Tag> getTags(@PathVariable Integer type) {
         return tagService.getByType(type);
     }
